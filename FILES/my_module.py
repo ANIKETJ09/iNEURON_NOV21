ee82eb11faaa240db23e598ee76f9e59630d4003
ANIKET_JOSHI <95517876+ANIKETJ09@users.noreply.github.com>
2023-08-24 22:57:11 +0530

    Add files via upload

---
+++ FILES/my_module.py
@@ -0,0 +1,9 @@
+data={"name":"ineuron",
+      "course":["data Science","blockchain","drone","robotics","cloud"],
+      "greeting":"Greeting from ineuron"}
+
+def get_course():
+    return data["course"]
+
+def greetings():
+    return data["greeting"]
---
+++ FILES/test1.py
@@ -0,0 +1,17 @@
+# Save the file after writng the code.
+
+def test(a,b):
+    return a+b
+
+def test1(a,b):
+  """This is the function which gives multiplication of 2 no."""
+    return a*b
+
+def test2(a,b):
+    return a-b
+
+def test3(a,b):
+    return a/b
+  
+ 
+
